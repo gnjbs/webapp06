169ffc33807678e7f23c2acc5db844981f9a69c4
ArturDS <arturdsoma@gmail.com>
2015-09-25 19:47:47 +0300

    hw1 rty

--- src/main/ru/javawebinar/webapp/model/Properties.java
+++ src/main/ru/javawebinar/webapp/model/Properties.java
@@ -6,22 +6,5 @@ package main.ru.javawebinar.webapp.model;
 public abstract class Properties  {
     private String name;
    // private String years;
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     private String description;
 }
